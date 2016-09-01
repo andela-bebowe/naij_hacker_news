e6364dfd15ebde7b2e2138ad964f5d5a72dac5be
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2016-09-01 11:56:02 +0100

    Set state from componentDidMount function

--- src/Container.js
+++ src/Container.js
@@ -4,12 +4,13 @@ import Body from './Body.js';
 class Container extends React.Component {
   constructor(props) { 
     super(props) ;
-    this.state = { "/": "The home page", "/arts": "The arts page", "/sports": "The sports page", "/politics": "The politics page", "/tech": "The tech page"};
+    this.state = {};
   }
   render() {
     return (<Body data={this.state[this.props.location.pathname]} />);
   }
   componentDidMount() {
+    this.setState({ "/": "The sethome page", "/arts": "The setarts page", "/sports": "The setsports page", "/politics": "The setpolitics page", "/tech": "The settech page"})
   }
 }
 
