c7b383f32f90501099972383539d6b47050c8fb7
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2016-09-05 11:59:18 +0100

    Include data in view.

--- src/Body.js
+++ src/Body.js
@@ -3,9 +3,14 @@ import NewsItem from './NewsItem.js'
 
 class Body extends React.Component {
   render() {
+    let newsData = this.props.newsData.hits;
+
     return (
       <div className="container">
-        <h2>Hi I am the body I am at {this.props.pathData}</h2>
+        <h4>Hi I am the body I am at {this.props.pathData}</h4>
+        <ul className="collection">
+          {newsData.map(item => <NewsItem key={item.objectID} item={item}/>)}
+        </ul>
       </div>
     );
   }
--- src/NewsItem.js
+++ src/NewsItem.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 class NewsItem extends React.Component {
   render() {
+    const item = this.props.item;
     return (
-      <li className="item">
-        
+      <li className="collection-item">
+        <span className="title">{item.title}</span>
+        <div className="item-info">
+          <p>{item.author}</p>
+          <p>Points: {item.points}</p>
+          <a href="#!" className=""><i className="material-icons">grade</i></a>
+        </div>
       </li>
     );
   }
