46f8ef5ec139fd665bf1e90e22595b108a834096
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2016-09-13 21:39:34 +0100

    Set up redux actions

---
+++ src/actions/index.js
@@ -0,0 +1,59 @@
+import fetch from 'isomorphic-fetch'
+
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const SELECT_CATEGORY = 'SELECT_CATEGORY'
+
+function requestPosts(query) {
+  return {
+    type: REQUEST_POSTS,
+    query
+  }
+}
+
+function receivePosts(category, posts) {
+  return {
+    type: RECEIVE_POSTS,
+    category,
+    posts,
+    receivedAt: Time.now()
+  }
+}
+
+export function selectCategory(category) {
+  return {
+    type: SELECT_CATEGORY,
+    category
+  }
+}
+
+function fetchPosts(query) {
+  let req_query = "Africa, Nigeria ", query
+  return dispatch => {
+    dispatch(requestPosts(query))
+    return fetch(`http://hn.algolia.com/api/v1/search_by_date?query=${req_query}`)
+      .then(response => response.json())
+      .then(posts => dispatch(receivePosts(query, posts)))
+  }
+}
+
+function shouldFetchPosts(state, category) {
+  const posts = state.postsByHN[category]
+  if (!posts) {
+    return true
+  }
+  if (posts.isFetching) {
+    return false
+  }
+  else if (posts.receivedAt < (Time.now() - 3600) {
+    return true
+  }
+}
+
+export function fetchPostsIfNeeded(query) {
+  return (dispatch, getState) => {
+    if (shouldFetchPosts(getState(), query)) {
+      return dispatch(fetchPosts(query))
+    }
+  }
+}
